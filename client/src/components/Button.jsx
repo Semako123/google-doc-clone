aa41c9159a379eab409749780ebbdc17002cab0f
semako123 <zosusemenoch@gmail.com>
2024-11-07 19:17:43 +0100

    remove classname library

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,11 +1,7 @@
 import React from "react";
-import classNames from "classnames";
 
 const Button = ({ children, variant, onClick, className }) => {
   const isOption = variant === "option";
-  const btnClass = classNames({
-    "": isOption,
-  });
   return (
     <div
       className={`${btnClass} w-fit flex gap-x-2 items-center hover:cursor-pointer text-sm border-transparent border font-normal text-[#202124] hover:bg-[#E8EBEE] rounded px-2 py-[2px] ${className}`}
