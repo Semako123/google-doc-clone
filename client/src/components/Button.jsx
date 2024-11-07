bf9b5c27d70bd29ac781bfbd4a202fd383e5d49f
semako123 <zosusemenoch@gmail.com>
2024-11-07 19:30:47 +0100

    Fixed minor bugs

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Button = ({ children, variant, onClick, className }) => {
-  const isOption = variant === "option";
+
   return (
     <div
-      className={`${btnClass} w-fit flex gap-x-2 items-center hover:cursor-pointer text-sm border-transparent border font-normal text-[#202124] hover:bg-[#E8EBEE] rounded px-2 py-[2px] ${className}`}
+      className={`w-fit flex gap-x-2 items-center hover:cursor-pointer text-sm border-transparent border font-normal text-[#202124] hover:bg-[#E8EBEE] rounded px-2 py-[2px] ${className}`}
       onClick={onClick}
     >
       {children}
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,6 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
