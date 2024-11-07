ebec6a2f89b9e728253f4511b22aa688cda55993
semako123 <zosusemenoch@gmail.com>
2024-11-07 19:22:41 +0100

    Made a silly error

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -7,17 +7,17 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDZcFNJCh6MqTwLGZnB41KrdA9-g5gzzB8",
-  authDomain: "doc-clone-be4f5.firebaseapp.com",
-  projectId: "doc-clone-be4f5",
-  storageBucket: "doc-clone-be4f5.firebasestorage.app",
-  messagingSenderId: "961131799768",
-  appId: "1:961131799768:web:b8e32ffa885953b15fe3ad",
-  measurementId: "G-8295BY9MCW",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export default app
+export default app;
